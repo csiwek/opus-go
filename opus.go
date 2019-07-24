f296918c9cdaf449d54a20a90a177ef84cea1e29
Cezary Siwek <cezary.siwek@gmail.com>
2019-07-24 14:34:24 +0100

    return payload

--- opus.go
+++ opus.go
@@ -39,11 +39,11 @@ func NewFile(fileName string) (*OpusReader, error) {
 	//}
 	reader.fd = f
 	reader.stream = bufio.NewReader(f)
-	_, err := i.getPage()
+	_, err = reader.getPage()
 	if err != nil {
 		return reader, err
 	}
-	_, err = i.getPage()
+	_, err = reader.getPage()
 	if err != nil {
 		return reader, err
 	}
@@ -123,32 +123,32 @@ func (i *OpusReader) readOpusTags() (uint32, error) {
 
 }
 
-func (i *OpusReader) getPage() (uint8, error) {
-
+func (i *OpusReader) getPage() ([]byte, error) {
+	payload = make([]byte, 255)
 	head := make([]byte, 4)
 	if err := binary.Read(i.stream, binary.LittleEndian, &head); err != err {
-		return 0, err
+		return payload, err
 	}
 	if bytes.Compare(head, []byte("OggS")) != 0 {
-		return 0, fmt.Errorf("Incorrect page. Does not start with \"OggS\" : %s %v", string(head), hex.EncodeToString(head))
+		return payload, fmt.Errorf("Incorrect page. Does not start with \"OggS\" : %s %v", string(head), hex.EncodeToString(head))
 	}
 	//Skipping Version
 	io.CopyN(ioutil.Discard, i.stream, 1)
 	var headerType uint8
 	if err := binary.Read(i.stream, binary.LittleEndian, &headerType); err != err {
-		return 0, err
+		return payload, err
 	}
 	fmt.Printf("headerType: %v\n", headerType)
 	if err := binary.Read(i.stream, binary.LittleEndian, &i.granulePosition); err != err {
-		return 0, err
+		return payload, err
 	}
 	fmt.Printf("i.granulePosition: %v\n", i.granulePosition)
 	if err := binary.Read(i.stream, binary.LittleEndian, &i.serial); err != err {
-		return 0, err
+		return payload, err
 	}
 	fmt.Printf("i.serial: %v\n", i.serial)
 	if err := binary.Read(i.stream, binary.LittleEndian, &i.pageIndex); err != err {
-		return 0, err
+		return payload, err
 	}
 	fmt.Printf("i.pageIndexl: %v\n", i.pageIndex)
 
@@ -157,13 +157,13 @@ func (i *OpusReader) getPage() (uint8, error) {
 
 	var segments uint8
 	if err := binary.Read(i.stream, binary.LittleEndian, &segments); err != err {
-		return 0, err
+		return payload, err
 	}
 	var payloadLen uint32
 	for x := 1; x <= int(segments); x++ {
 		var segSize uint8
 		if err := binary.Read(i.stream, binary.LittleEndian, &segSize); err != err {
-			return 0, err
+			return payload, err
 		}
 		fmt.Printf("Seg %d size: %v\n", x, segSize)
 		payloadLen = payloadLen + uint32(segSize)
@@ -173,7 +173,7 @@ func (i *OpusReader) getPage() (uint8, error) {
 	fmt.Printf("segments: %v\n", segments)
 	magic := make([]byte, 8)
 	if err := binary.Read(i.stream, binary.LittleEndian, &magic); err != err {
-		return 0, err
+		return payload, err
 	}
 	if bytes.Compare(magic, []byte("OpusHead")) == 0 {
 		plen, err := i.readOpusHead()
@@ -191,16 +191,16 @@ func (i *OpusReader) getPage() (uint8, error) {
 		io.CopyN(ioutil.Discard, i.stream, int64(payloadLen-30))
 	} else {
 		fmt.Printf("an audio frame\n")
-		io.CopyN(ioutil.Discard, i.stream, int64(payloadLen-8))
+		io.CopyN(payload, i.stream, int64(payloadLen-8))
 	}
 
 	fmt.Printf("payloadLen: %v\n", payloadLen)
 
-	return headerType, nil
+	return payload, nil
 }
 
 func (i *OpusReader) GetSample() ([]byte, error) {
-	payload, err = i.getPage()
+	payload, err := i.getPage()
 	if err != nil {
 		return nil, err
 	}
