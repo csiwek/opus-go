96622fa6ad654b609ebacb80151dba4bdbbd31b4
Cezary Siwek <cezary.siwek@gmail.com>
2019-07-30 20:42:31 +0100

    TOC stereo flag

--- opus.go
+++ opus.go
@@ -281,10 +281,14 @@ func (i *OpusReader) getPageSingle() ([]byte, error) {
 		//shift 3 bits right to get a value of 5 leading bits. See https://tools.ietf.org/html/rfc6716
 		toc := tmpPacket[0] >> 3
 		i.currentSampleLen = getFrameSize(uint8(toc))
-		tocStereo := tmpPacket[0] & (1 << 5)
+		tocStereo := hasBit(tmpPacket[0],5)
 		fmt.Printf("Toc Stereo : %v\n", tocStereo)
 	}
 	return tmpPacket, nil
+}
+func hasBit(n byte, pos uint8) bool {
+    val := n & (1 << pos)
+    return (val > 0)
 }
 
 //Returns Frame size in ms based on Configuration number
